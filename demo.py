1e5596a0f6ee17466000efe1986df311b3d99255
MilindRathod <rmilind924@gmail.com>
2022-11-01 09:22:45 +0530

    jh

--- demo.py
+++ demo.py
@@ -1,1 +1,11 @@
-print("hello")
+import imp
+import tkinter
+from tkinter import *
+pane = Tk()
+b1 = Button(pane, text = "Click me !")
+b1.pack(fill=Y, expand = True)
+# Button 2
+b2 = Button(pane, text = "Click me too")
+b2.pack(fill = X, expand = True)
+# Execute Tkinter
+pane.mainloop()
